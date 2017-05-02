652f72f932dc7a4e1704381bdc989dbd447151f9
Vinda Fadilla <vinda.fadilla.tif14@polban.ac.id>
2017-05-02 17:00:53 +0700

    Add files via upload

---
+++ tfidf.py
@@ -0,0 +1,32 @@
+import math
+from textblob import TextBlob as tb
+
+def tf(word, blob):
+    return blob.words.count(word) / len(blob.words)
+
+def n_containing(word, bloblist):
+    return sum(1 for blob in bloblist if word in blob)
+
+def idf(word, bloblist):
+    return math.log(len(bloblist) / (1 + n_containing(word, bloblist)))
+
+def tfidf(word, blob, bloblist):
+    return tf(word, blob) * idf(word, bloblist)
+
+document1 = tb("""Yeah. Starbucks makes my life perfect. Love Machiato.""")
+
+document2 = tb("""Python, from the Greek word (πύθων/πύθωνας), is a genus of
+nonvenomous pythons[2] found in Africa and Asia. Currently, 7 species are
+recognised.[2] A member of this genus, P. reticulatus, is among the longest
+snakes known.""")
+
+document3 = tb("""I went to Starbucks with Kai. Then I realized, he's so cute,
+               when he drank Machiato. Love Machiato so much.""")# jijay -_-
+
+bloblist = [document1, document2, document3]
+for i, blob in enumerate(bloblist):
+    print("Top words in document {}".format(i + 1))
+    scores = {word: tfidf(word, blob, bloblist) for word in blob.words}
+    sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+    for word, score in sorted_words[:3]:
+        print("Word: {}, TF-IDF: {}".format(word, round(score, 5)))
